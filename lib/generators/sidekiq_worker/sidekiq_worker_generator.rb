f7bbed33f2812cc316005021f5fa42e1295dc6a4
Clabybite <clabybite@gmail.com>
2025-06-14 13:35:38 +0300

    fix sidekiq generator

--- lib/generators/sidekiq_worker/sidekiq_worker_generator.rb
+++ lib/generators/sidekiq_worker/sidekiq_worker_generator.rb
@@ -1,17 +1,15 @@
-require "rails/generators"
+# lib/generators/sidekiq_worker/sidekiq_worker_generator.rb
+module SidekiqWorker
+  class SidekiqWorkerGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path("templates", __dir__)
+    desc "Generates a Sidekiq worker and its test"
 
-module Generators
-  module SidekiqWorker
-    class SidekiqWorkerGenerator < Rails::Generators::NamedBase
-      source_root File.expand_path("templates", __dir__)
+    def create_worker_file
+      template "worker.rb.tt", File.join("app/workers", class_path, "#{file_name}.rb")
+    end
 
-      def create_worker_file
-        template "worker.rb.tt", File.join("app/workers", class_path, "#{file_name}.rb")
-      end
-
-      def create_test_file
-        template "worker_test.rb.tt", File.join("test/workers", class_path, "#{file_name}_test.rb")
-      end
+    def create_test_file
+      template "worker_test.rb.tt", File.join("test/workers", class_path, "#{file_name}_test.rb")
     end
   end
 end
