c902a33c06d97d66f099d67b427822db656c152c
Clabybite <clabybite@gmail.com>
2025-06-17 13:19:18 +0300

    fix templating for sidekiq initializer undefined local variable or method `db' for an instance of SidekiqWorker::SidekiqWorkerGenerator (NameError)
                        Rails.logger.info "[Sidekiq] Assigned Redis DB ##{db} to #{APP_ID}"

--- lib/generators/shared/sidekiq_support.rb
+++ lib/generators/shared/sidekiq_support.rb
@@ -28,42 +28,7 @@ module Shared
     end
 
     def create_sidekiq_initializer
-      create_file "config/initializers/sidekiq.rb", <<~RUBY
-        # config/initializers/sidekiq.rb
-        require 'sidekiq'
-        require 'redis'
-        require 'zlib'
-
-        APP_ID = "#{Rails.application.class.module_parent_name rescue 'default_app'}:#{Rails.env}"
-        REDIS_HOST = ENV.fetch("REDIS_HOST", "localhost")
-        REDIS_PORT = ENV.fetch("REDIS_PORT", "6379")
-        MAX_DB = 15
-
-        def find_or_assign_redis_db
-            (0..MAX_DB).each do |db|
-                redis = Redis.new(host: REDIS_HOST, port: REDIS_PORT, db: db)
-                marker = redis.get("sidekiq_app_id")
-
-                return db if marker == APP_ID
-
-                if marker.nil?
-                    redis.set("sidekiq_app_id", APP_ID)
-                    Rails.logger.info "[Sidekiq] Assigned Redis DB ##{db} to #{APP_ID}"
-                    return db
-                end
-            end
-
-            raise "No free Redis DB available (0..#{MAX_DB} already taken)"
-        end
-
-        redis_db = ENV["REDIS_DB"] || find_or_assign_redis_db
-        redis_url = "redis://#{REDIS_HOST}:#{REDIS_PORT}/#{redis_db}"
-
-        Sidekiq.configure_server { |config| config.redis = { url: redis_url } }
-        Sidekiq.configure_client { |config| config.redis = { url: redis_url } }
-
-        Rails.logger.info "[Sidekiq] '#{APP_ID}' using Redis DB ##{redis_db}"
-      RUBY
+      template "sidekiq_initializer.rb.tt", "config/initializers/sidekiq.rb"
     end
 
     def create_sidekiq_config
